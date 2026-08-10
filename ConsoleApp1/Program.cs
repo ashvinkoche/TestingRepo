c57c22f75a820da4dfabe35aab54817b16051a33
Ashvin Koche <ashvin.koche@bbconsult.co.uk>
2026-08-10 16:07:30 +0530

    Add initial test code

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting application...");
+
+            var calculator = new Calculator();
+
+            var result = calculator.Add(10, 20);
+
+            Console.WriteLine("Result: " + result);
+
+            var userName = GetUserName();
+
+            if (userName != null)
+            {
+                Console.WriteLine("Hello " + userName);
+            }
+
+            Console.WriteLine("Application finished.");
+        }
+
+        static string GetUserName()
+        {
+            return "Adam";
+        }
+    }
+
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            var result = a + b;
+
+            return result;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            var result = a * b;
+
+            return result;
+        }
+    }
+}
+
